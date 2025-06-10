c826fe083f705d3b1b77ec889c48138614ae5359
onlyVishesh <visheshmaurya14@gmail.com>
2025-06-10 23:35:43 +0530

    fixed logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       className={`header-container flex items-center justify-between px-6 py-4 fixed top-0 w-full z-50 transition-all duration-300 ${"bg-white/80 backdrop-blur-lg border-b border-gray-200/50 shadow-sm"}`}
     >
       <div className="flex items-center">
-        <img src="./public/logo.png" alt="logo" className="size-10" />
+        <img src="/logo.png" alt="logo" className="size-10" />
         <Link to="/" preventScrollReset={true} className="no-underline">
           <div
             className={`ml-3 text-xl font-bold transition-colors ${"text-gray-900 hover:text-blue-600"}`}
